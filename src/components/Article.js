90c4372774c5422eb5c0e222dd4adbfaa51a0eb3
AbinayaaNagarajan <abinayaanagarajan@gmail.com>
2024-01-29 12:16:05 -0500

    Reactblog new

---
+++ src/components/Article.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function Article() {
+  return (
+    <main>
+      <article>
+        <h2>Blog Post Title 1</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+        <img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-1.jpg" alt="Description of Image 1" />
+        <p>Continues... <a href="#">Read more</a></p>
+      </article>
+
+      <article>
+        <h2>Blog Post Title 2</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. ...</p>
+        <img src="https://ps-react-curriculum.herokuapp.com/fashionblog/blog-image-2.jpg" alt="Description of Image 2" />
+        <p>Continues... <a href="#">Read more</a></p>
+      </article>
+    </main>
+  );
+}
+
+export default Article;
---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+function Nav() {
+  return (
+    <nav aria-label="Main Navigation" role="navigation">
+      <ul className="main_nav">
+      <li><a href="#">Women's</a></li>
+      <li><a href="#">Men's</a></li>
+      <li><a href="#">On the Street</a></li>
+      <li><a href="#">The Catwalk</a></li>
+      <li><a href="#">Ad Watch</a></li>
+      <li><a href="#">About</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Nav;
